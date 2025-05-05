c23851762f882c595c10d34e01540457f54df818
Changeline <thierry.dubosson@gmail.com>
2025-05-05 11:01:43 +0200

    Update installKDEGUI.sh
    Added some comments

--- installKDEGUI.sh
+++ installKDEGUI.sh
@@ -1,3 +1,4 @@
+# I went the HyDE route for my GUI, it was nice setting everything from ground up, but HyDE looks and feel better. 
 sudo pacman -Syu
 sudo pacman -S xorg plasma kde-applications sddm
 sudo systemctl enable sddm
